85efed5a3ca9e5d4aa42b32aa32daa27197525da
songhyunji <37237094+songhyunji@users.noreply.github.com>
2019-04-12 00:47:24 +0900

    hp bar, progress bar
    hp 바와 진행상황 바 구현

--- Assets/_송현지/Enemy.cs
+++ Assets/_송현지/Enemy.cs
@@ -2,20 +2,23 @@
 using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.EventSystems;
+using UnityEngine.UI;
 
 public class Enemy : MonoBehaviour {
-
-	private Transform _transform;
-
+	
 	public float moveSpeed;
 	public GameObject player;
+	[Tooltip("진행 상황 바 스크립트")]
+	public ProgressBar_ progressBarScript;
 
+	private Transform _transform;
 	private Transform playerTransform;
 
 	// Use this for initialization
 	void Start () {
 		_transform = GetComponent<Transform>();
 		playerTransform = player.GetComponent<Transform>();
+		progressBarScript = GameObject.FindGameObjectWithTag("ProgressBar").GetComponent<ProgressBar_>();
 	}
 	
 	// Update is called once per frame
@@ -31,6 +34,7 @@ public class Enemy : MonoBehaviour {
 
 	private void OnMouseDown()
 	{
+		progressBarScript.currentAmount--;
 		Destroy(this.gameObject);
 		Debug.Log("click");
 	}
--- Assets/_송현지/player.cs
+++ Assets/_송현지/player.cs
@@ -1,19 +1,26 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
 
-public class player : MonoBehaviour {
+public class Player : MonoBehaviour {
 
 	public Joystick joystick;
 	public float moveSpeed;
+	public GameObject hpSlider;
+	[Tooltip("적의 공격 피해량 (0-1)")]
+	public float damage = 0.05f;
 
 	private Vector3 moveVector;
 	private Rigidbody _rigidbody;
-	
+	private Slider hpSliderScript;
+	private Slider progressSliderScript;
+
 
 	private void Start()
 	{
 		_rigidbody = GetComponent<Rigidbody>();
+		hpSliderScript = hpSlider.GetComponent<Slider>();
 	}
 
 	private void Update()
@@ -32,5 +39,13 @@ public class player : MonoBehaviour {
 
 
 		return dir;
+	}
+
+	private void OnCollisionStay(Collision collision)
+	{
+		if (collision.gameObject.CompareTag("Enemy"))
+		{
+			hpSliderScript.value -= damage;
+		}
 	}
 }
