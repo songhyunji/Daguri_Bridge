7bdbd6b76363143f5f603b9a1579688c2cff02c9
songhyunji <37237094+songhyunji@users.noreply.github.com>
2019-05-24 22:32:31 +0900

    delete all

--- Assets/QuizGameExample/Scripts/DelayDestroy.cs
+++
@@ -1,9 +0,0 @@
-using UnityEngine;
-using System.Collections;
-
-public class DelayDestroy : MonoBehaviour {
-	public float delayTime = 2f;
-	void Start () {
-		Destroy(gameObject, delayTime);
-	}
-}
