831b7b14739208fdba9961a4d85bd501847e7740
songhyunji <37237094+songhyunji@users.noreply.github.com>
2019-07-11 21:04:33 +0900

    Main Scene
    메인 씬

---
+++ Assets/_Song/MainButtonManager.cs
@@ -0,0 +1,40 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using UnityEngine.SceneManagement;
+
+public class MainButtonManager : MonoBehaviour {
+
+	public Button gameStartBtn;
+	public Button weapon1Btn;
+	public Button weapon2Btn;
+	public Button settingBtn;
+
+	private bool gameStartBtnOn = false;
+
+	public void gameStartBtnPress()
+	{
+		if(gameStartBtnOn)
+		{
+			gameStartBtnOn = false;
+		}
+		else
+		{
+			gameStartBtnOn = true;
+		}
+		weapon1Btn.gameObject.SetActive(gameStartBtnOn);
+		weapon2Btn.gameObject.SetActive(gameStartBtnOn);
+		settingBtn.GetComponent<Button>().enabled = !gameStartBtnOn;
+	}
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+}
---
+++ Assets/_Song/RotatePlayer.cs
@@ -0,0 +1,15 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class RotatePlayer : MonoBehaviour {
+
+	float rotSpeed = 100;
+
+	void OnMouseDrag()
+	{
+		float rotX = Input.GetAxis("Mouse X") * rotSpeed * Mathf.Deg2Rad;
+
+		transform.Rotate(Vector3.up, -rotX);
+	}
+}
